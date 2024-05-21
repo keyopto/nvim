0ee410a651e38bc85ac012c187763104324a3ddc
Keyopto <keyopto@gmail.com>
2024-05-21 22:35:25 +0200

    feat: few remaps and set

--- lua/keyopto/remap.lua
+++ lua/keyopto/remap.lua
@@ -4,6 +4,12 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
 vim.keymap.set('n', '<A-h>', [[<Cmd>wincmd h<CR>]])
 vim.keymap.set('n', '<A-j>', [[<Cmd>wincmd j<CR>]])
 vim.keymap.set('n', '<A-k>', [[<Cmd>wincmd k<CR>]])
@@ -24,3 +30,9 @@ vim.keymap.set('v', '<Tab>', '>')
 vim.keymap.set('v', '<S-Tab>', '<')
 
 vim.keymap.set('n', '<leader>wt', '<cmd>set wrap!<CR>')
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
--- lua/keyopto/set.lua
+++ lua/keyopto/set.lua
@@ -23,3 +23,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
