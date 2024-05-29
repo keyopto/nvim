aeeb578f4efca31a62aa6714c116ae11353f512f
Keyopto <keyopto@gmail.com>
2024-05-29 21:18:46 +0200

    feat: change to mason lspconfig handlers configuration

--- after/plugin/lsp_zero.lua
+++ after/plugin/lsp_zero.lua
@@ -7,46 +7,37 @@ require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
     completion = {
-    completeopt = "menu,menuone,preview,noselect",
-  },
-  snippet = { -- configure how nvim-cmp interacts with snippet engine
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-  }),
-  -- sources for autocompletion
-  sources = cmp.config.sources({
-    { name = "nvim_lsp"},
-    { name = "luasnip" }, -- snippets
-    { name = "buffer" }, -- text within current buffer
-    { name = "path" }, -- file system paths
-  }),
-
-  -- configure lspkind for vs-code like pictograms in completion menu
-  -- formatting = {
-  --   format = lspkind.cmp_format({
-  --     maxwidth = 50,
-  --     ellipsis_char = "...",
-  --   }),
-  -- },
-})
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        'rust_analyzer',
-        'lua_ls',
-        'tsserver',
+        completeopt = "menu,menuone,preview,noselect",
     },
+    snippet = { -- configure how nvim-cmp interacts with snippet engine
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    }),
+    -- sources for autocompletion
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- snippets
+        { name = "buffer" },  -- text within current buffer
+        { name = "path" },    -- file system paths
+    }),
+
+    -- configure lspkind for vs-code like pictograms in completion menu
+    -- formatting = {
+    --   format = lspkind.cmp_format({
+    --     maxwidth = 50,
+    --     ellipsis_char = "...",
+    --   }),
+    -- },
 })
 
 local on_attach = function(_, _)
@@ -56,24 +47,43 @@ local on_attach = function(_, _)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
     vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-    vim.keymap.set('n', 'K',  vim.lsp.buf.hover, {})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require('lspconfig')
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").tsserver.setup {
-    on_attach = on_attach,
-    capabilities = capabilities
-}
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'rust_analyzer',
+        'lua_ls',
+        'tsserver',
+    },
+    handlers = {
+        function(server)
+            lspconfig[server].setup({
+                capabilities = lsp_capabilities,
+                on_attach = on_attach
+            })
+        end,
+        ['tsserver'] = function()
+            lspconfig.tsserver.setup({
+                capabilities = lsp_capabilities,
+                on_attach = on_attach,
+                settings = {
+                    completions = {
+                        completeFunctionCalls = true
+                    }
+                }
+            })
+        end,
+    }
+})
 
-require("lspconfig").lua_ls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities
-}
 
 local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
