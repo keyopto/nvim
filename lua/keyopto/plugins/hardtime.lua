175662af200b838397dd65e5626bfee583a6df3b
Keyopto <keyopto@gmail.com>
2024-06-13 21:47:57 +0200

    feat: get rid of hardtime (temporarly perhaps)

--- lua/keyopto/plugins/hardtime.lua
+++ lua/keyopto/plugins/hardtime.lua
@@ -1,9 +1,9 @@
 return {
-	{
-		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
+	-- {
+	-- 	"m4xshen/hardtime.nvim",
+	-- 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	-- 	opts = {},
+	-- },
 	{
 		"tris203/precognition.nvim",
 		--event = "VeryLazy",
