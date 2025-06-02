a07bdb15e57214657ec8b343e3aebe60f92e60b6
keyopto <keyopto@gmail.com>
2025-06-02 14:48:46 +0200

    feat: add telescope ui select (for code actions)

--- lua/keyopto/plugins/navigation.lua
+++ lua/keyopto/plugins/navigation.lua
@@ -1,39 +1,41 @@
 return {
 
-	-- telescope -> how to go to every file wi
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+  -- telescope -> how to go to every file wi
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
-	-- navigate between buffers
-	{
-		"theprimeagen/harpoon",
-	},
+  { 'nvim-telescope/telescope-ui-select.nvim' },
 
-	-- to see the tree organization of the repository
-	{
-		"antosha417/nvim-lsp-file-operations",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			"nvim-neo-tree/neo-tree.nvim",
-		},
-		config = function()
-			require("lsp-file-operations").setup()
-		end,
-	},
+  -- navigate between buffers
+  {
+    "theprimeagen/harpoon",
+  },
 
-	-- {
-	--     "nvim-neo-tree/neo-tree.nvim",
-	--     branch = "v3.x",
-	--     dependencies = {
-	--         "nvim-lua/plenary.nvim",
-	--         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-	--         "MunifTanjim/nui.nvim",
-	--         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	--     }
-	-- }
+  -- to see the tree organization of the repository
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+
+  -- {
+  --     "nvim-neo-tree/neo-tree.nvim",
+  --     branch = "v3.x",
+  --     dependencies = {
+  --         "nvim-lua/plenary.nvim",
+  --         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+  --         "MunifTanjim/nui.nvim",
+  --         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  --     }
+  -- }
 }
